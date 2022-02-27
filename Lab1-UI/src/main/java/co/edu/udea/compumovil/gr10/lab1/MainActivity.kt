98111655837769d76d5d7fa628de5eb6f2558576
Juan <juan.garcia142@udea.edu.co>
2022-02-26 21:53:04 -0500

    Se corrige problema de App fallando en modo horizontal.

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr10/lab1/MainActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr10/lab1/MainActivity.kt
@@ -2,21 +2,26 @@ package co.edu.udea.compumovil.gr10.lab1
 
 import android.app.DatePickerDialog
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import java.text.SimpleDateFormat
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // access the items of the list
         val languages = resources.getStringArray(R.array.Languages)
+
 
         // access the spinner
         val spinner = findViewById<Spinner>(R.id.spinner)
